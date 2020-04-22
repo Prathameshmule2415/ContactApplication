d303ed7d7b580849e68f511b152783e19da19f3c
Prathameshmule2415 <59178753+Prathameshmule2415@users.noreply.github.com>
2020-04-22 22:47:59 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyAVwv7132bV2oON3R1E-H5CuFyLTGhS_ZA",
+  authDomain: "reactcontactapp-10b8a.firebaseapp.com",
+  databaseURL: "https://reactcontactapp-10b8a.firebaseio.com",
+  projectId: "reactcontactapp-10b8a",
+  storageBucket: "reactcontactapp-10b8a.appspot.com",
+  messagingSenderId: "566059096909",
+  appId: "1:566059096909:web:f6a9f99ee288d96ea83504",
+  measurementId: "G-324SYY01RQ",
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
